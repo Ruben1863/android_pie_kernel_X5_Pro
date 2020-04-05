96a5491e7291951688ef43431df1177c2a704cee
Ruben Espinola <rubenes2003@gmail.com>
2020-04-05 13:21:16 +0200

    Update drvgen.mk

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -70,6 +70,11 @@ $(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(PROJ_DTS_FILES)
 		fi \
 	done
 
+dtbo_check: 
+	echo "Eh"
+
+ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
+
 dtbo_check: $(MAIN_DTB_NAMES) $(PROJ_DTB_NAMES)
 	for i in $(PROJ_DTB_FILES); do \
 		$(srctree)/scripts/dtc/ufdt_apply_overlay $(MAIN_DTB_FILES) $$i $$i.merge;\
@@ -91,5 +96,5 @@ $(objtree)/dtboimg.cfg: FORCE
 	else \
 		rm $@.tmp; \
 	fi
-
+endif
 endif#MTK_PLATFORM
